31839bd7c3aca6708db08857cbf7957e02a7b16f
Manaswini Bhaskarpatla <64688923+Manaswini-BK@users.noreply.github.com>
2020-06-03 21:56:39 +0530

    Add files via upload

---
+++ editDistance.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+    const m = word1.length;
+    const n = word2.length;
+    
+    const dp = [];
+    
+    for (let i = 0; i <= m; i++) {
+        dp[i] = new Array(n + 1).fill(0);
+        dp[i][0] = i;
+    }
+    
+    for (let j = 1; j <= n; j++) {
+        dp[0][j] = j;
+    }
+    
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            // 2 cases - where the characters match and where it doesn't
+            if (word1.charAt(i - 1) === word2.charAt(j - 1)) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
+};
